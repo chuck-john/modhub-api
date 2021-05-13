95eb2ed87f35f9e30732a78ca3e7020ab1b83c8b
CJ Howard <charlesjhoward@pm.me>
2021-05-13 12:56:32 -0500

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  with_options defaults: { format: :json }, only: :index do
-    resources :makes
-    resources :models
-    resources :modes
-    resources :trims
-  end
+  with_options defaults: { format: :json } do
+    with_options only: :index do
+      resources :makes
+      resources :models
+      resources :modes
+      resources :trims
+    end
 
-  resource :sessions, only: %i[create destroy]
-  resource :users, only: %i[create update destroy]
+    resource :sessions, only: %i[create destroy]
+    resource :users, only: %i[create update destroy]
+  end
 end
